5878daf7a5729822e63fedc9b15f088eb0f290c5
Santiago Andaluz Ruiz <santiago_andaluz_ruiz@baylor.edu>
2018-03-29 17:09:02 +0000

    Starting point for ChefBot

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Version is a constant that store the version of ChefBot
+const Version = "v0.0.1-alpha"
+
+var (
+	// Token is the Discord Bot Token
+	Token string
+
+	// Router will be the multiplexer for all of the commands
+	Router = mux.New()
+)
+
+func init() {
+	Token = os.Getenv("DG_TOKEN")
+	if Token == "" {
+		flag.StringVar(&Token, "t", "", "Discord Authentication Token")
+		flag.Parse()
+	}
+}
+
+func main() {
+	dg, err := discordgo.New(Token)
+	if err != nil {
+		fmt.Println("Error creating Discrod session", err)
+		return
+	}
+
+	dg.AddHandler(Router.OnMessageCreate)
+
+	Router.Route("help", "Display this message", Router.Help)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!!")
+	}
+}
